32cbd7ed343b486ed19eb96f507816dbf032f782
ShakeelGadafi <shakeelgadhafi@gmail.com>
2025-12-16 18:26:17 +0530

    refacted the rooms code

--- controllers/rooms.controller.js
+++ controllers/rooms.controller.js
@@ -17,23 +17,22 @@ exports.createRoom = async (req, res) => {
   try{
   const {room_number, category, price, beds}= req.body;
 
-  if (!room_number || !category || price == null || beds == null) {
-      return res.status(400).json({ error: 'All fields are required' });
-  }  
-
-  if(price<=0){
-     return res.status(400).json({message:"Price cannot be negative"});
-  }
-
-  if(beds<=0){
-     return res.status(400).json({message:"Beds cannot be negative"});
-  }
+   if (!room_number) return res.status(400).json({ error: 'room_number is required' });
+    if (!category) return res.status(400).json({ error: 'category is required' });
+    if (price == null || price <= 0) return res.status(400).json({ error: 'price must be > 0' });
+    if (beds == null || beds <= 0) return res.status(400).json({ error: 'beds must be > 0' });
+    
+  
   const result = await pool.query(
       'INSERT INTO rooms (room_number, category, price, beds) VALUES ($1, $2, $3, $4) RETURNING *',
       [room_number, category, price, beds]
   );
   res.status(201).json(result.rows[0]);
+
   } catch (error) {
+    if(error.code === '23505'){
+      return res.status(400).json({ error: 'Room_number already exists' });
+    }
     console.error("error creating room:", error);
     res.status(500).json({ error: "Internal Server Error" });
   }
